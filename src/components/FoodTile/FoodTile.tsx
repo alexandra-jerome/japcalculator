4a65497306789336230f2d74af59b454b8faa7b3
Alexandra Jerome <alexandra.jerome@famillejerome.fr>
2021-05-11 17:17:00 +0200

    commit 3

--- src/components/FoodTile/FoodTile.tsx
+++ src/components/FoodTile/FoodTile.tsx
@@ -22,7 +22,7 @@ interface OwnProps {
 }
 
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://fake_user:fake_leak@cluster0.xybtu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = "mongodb+srv://fake_user:fakeleak@cluster0.xybtu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const collection = client.db("test").collection("devices");
